fc8cfd8ddb093141305fd8234cc68fb5f7e7cb5a
muflikhahisna <muflikhah.a@students.amikom.ac.id>
2018-11-27 12:18:50 +0700

    commit

---
+++ app/src/main/java/com/example/amikom/notification/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.amikom.notification;
+
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int NOTIF_ID = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button pencet = (Button) findViewById(R.id.btn_Send);
+
+        pencet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationCompat.Builder notifBuilder = (NotificationCompat.Builder) new
+                        NotificationCompat.Builder(MainActivity.this)
+                        .setSmallIcon(R.drawable.ic_access_alarms_black_36dp)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources()
+                                , R.drawable.ic_access_alarms_black_36dp))
+
+                        .setContentTitle(getResources().getString(R.string.content_title))
+
+                        .setContentTitle(getResources().getString(R.string.content_text))
+                        .setSubText(getResources().getString(R.string.subtext))
+                        .setAutoCancel(true);
+
+                NotificationManagerCompat ncm =
+                        NotificationManagerCompat.from(getApplicationContext());
+                ncm.notify(NOTIF_ID, notifBuilder.build());
+            }
+        });
+    }
+
+
+}
